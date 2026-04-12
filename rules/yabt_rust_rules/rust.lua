56547ccd80ff3868f9b5abd41830d86c89e4237b
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2026-04-12 23:29:47 +0200

    Support library crates

--- rules/yabt_rust_rules/rust.lua
+++ rules/yabt_rust_rules/rust.lua
@@ -1,20 +1,151 @@
 local M = {}
 
+local BIN_TYPE = 'bin'
+local LIB_TYPE = 'lib'
+local DEFAULT_EDITION = '2024'
+
 ---@param toolchain Toolchain
 ---@return BuildRule # Build rule for the given toolchain
-local function bin_rule_for_toolchain(toolchain)
+local function rule_for_toolchain(toolchain, type)
+    local crate_type
+    local name_suffix
+    if type == BIN_TYPE then
+        crate_type = 'bin'
+        name_suffix = 'rbin'
+    elseif type == LIB_TYPE then
+        crate_type = 'lib'
+        name_suffix = 'rlib'
+    end
     return {
-        name = toolchain.name .. '-rbin',
+        name = toolchain.name .. name_suffix,
         cmd = toolchain.rustc .. ' ' .. table.concat(toolchain.rustflags, ' ') ..
-            '--out-dir $out_dir --edition $edition --crate-name $crate_name --crate-type bin $in --emit dep-info,link',
-        descr = 'rustc (toolchain: ' .. toolchain.name .. ') $out',
+            '--out-dir $out_dir --edition $edition --crate-name $crate_name --crate-type ' ..
+            crate_type .. ' $rustflags $src --emit dep-info,link',
+        descr = 'rustc (toolchain: ' .. toolchain.name .. ') $target',
     }
+end
+
+---@param deps Dep[]
+local function collect_dependencies(toolchain, deps)
+    local ninja_input_deps = {}
+    local rustflags = ''
+
+    ---@type Library[]
+    local lib_queue = {}
+    for _, dep in ipairs(deps) do
+        table.insert(lib_queue, dep:rust_library(toolchain))
+    end
+
+    local handled = {}
+
+    while #lib_queue > 0 do
+        ---@type Library
+        local lib = table.remove(lib_queue, 1)
+
+        if handled[lib.crate_name] then
+            goto continue
+        end
+        handled[lib.crate_name] = true
+
+        table.insert(ninja_input_deps, lib:out_file())
+        rustflags = rustflags .. ' --extern '.. lib.crate_name .. ' -L ' .. lib.out_dir:absolute()
+
+        for _, dep in ipairs(lib.deps) do
+            table.insert(lib_queue, dep:rust_library(toolchain))
+        end
+
+        ::continue::
+    end
+
+    return ninja_input_deps, rustflags
+end
+
+---@class Dep
+---@field rust_library fun(self: Dep, toolchain: Toolchain): Library
+
+---@class Library
+---@field crate_name string
+---@field out_dir OutPath
+---@field src Path
+---@field deps ?Dep[]
+---@field edition string
+---@field rustflags ?string[]
+---@field toolchain ?Toolchain
+local Library = {}
+
+---@param lib Library
+function Library:new(lib)
+    setmetatable(lib, self)
+    self.__index = self
+    lib:resolve()
+    return lib
+end
+
+-- Resolves the missing binary bits at lib declaration time
+function Library:resolve()
+    local selected_toolchain = require 'yabt_rust_rules.toolchain'.selected_toolchain()
+    self.toolchain = self.toolchain or selected_toolchain
+    self.rustflags = self.rustflags or {}
+    self.deps = self.deps or {}
+
+    local path = require 'yabt.core.path'
+    if not path.is_out_path(self.out_dir) then
+        error('Rust binary requires an output directory: ' .. type(self.src), 3)
+    end
+
+    if not path.is_path(self.src) then
+        error('Rust binary requires a source file: ' .. type(self.src), 3)
+    end
+
+    if type(self.crate_name) ~= "string" or self.crate_name == '' then
+        error('Rust binary requires a crate name', 3)
+    end
+end
+
+---@return OutPath
+function Library:out_file()
+    return self.out_dir:join('lib' .. self.crate_name .. '.rlib')
+end
+
+---@param ctx Context
+function Library:build(ctx)
+    local out_file = self:out_file()
+    local dep_file = self.out_dir:join(self.crate_name .. '.d')
+    local ninja_input_deps, rustflags = collect_dependencies(self.toolchain, self.deps)
+    table.insert(ninja_input_deps, self.src)
+
+    for _, flag in ipairs(self.rustflags) do
+        rustflags = rustflags .. ' ' .. flag
+    end
+
+    local build_rule = rule_for_toolchain(self.toolchain, LIB_TYPE)
+    local build_step = {
+        outs = { dep_file, out_file },
+        ins = ninja_input_deps,
+        rule_name = build_rule.name,
+        variables = {
+            edition = self.edition or DEFAULT_EDITION,
+            crate_name = self.crate_name,
+            out_dir = self.out_dir:absolute(),
+            depfile = dep_file:absolute(),
+            rustflags = rustflags,
+            src = self.src:absolute(),
+            target = out_file:absolute()
+        }
+    }
+    ctx.add_build_step_with_rule(build_step, build_rule)
+end
+
+---@return Library
+function Library:rust_library(toolchain)
+    return self
 end
 
 ---@class Binary
 ---@field crate_name string
 ---@field out_dir OutPath
 ---@field src Path
+---@field deps Dep[]
 ---@field edition string
 ---@field rustflags ?string[]
 ---@field toolchain ?Toolchain
@@ -32,6 +163,8 @@ end
 function Binary:resolve()
     local selected_toolchain = require 'yabt_rust_rules.toolchain'.selected_toolchain()
     self.toolchain = self.toolchain or selected_toolchain
+    self.deps = self.deps or {}
+    self.rustflags = self.rustflags or {}
 
     local path = require 'yabt.core.path'
     if not path.is_out_path(self.out_dir) then
@@ -47,28 +180,42 @@ function Binary:resolve()
     end
 end
 
+function Binary:out_file()
+    return self.out_dir:join(self.crate_name)
+end
+
 ---@param ctx Context
 function Binary:build(ctx)
-    local ins = { self.src }
-    local build_rule = bin_rule_for_toolchain(self.toolchain)
-    local out_file = self.out_dir:join(self.crate_name)
+    local out_file = self:out_file()
     local dep_file = out_file:with_ext('d')
+    local ninja_input_deps, rustflags = collect_dependencies(self.toolchain, self.deps)
+    table.insert(ninja_input_deps, self.src)
+
+    for _, flag in ipairs(self.rustflags) do
+        rustflags = rustflags .. ' ' .. flag
+    end
+
+    local build_rule = rule_for_toolchain(self.toolchain, BIN_TYPE)
     local build_step = {
         outs = { dep_file, out_file },
-        ins = ins,
+        ins = ninja_input_deps,
         rule_name = build_rule.name,
         variables = {
-            edition = self.edition or '2024',
+            edition = self.edition or DEFAULT_EDITION,
             crate_name = self.crate_name,
             out_dir = self.out_dir:absolute(),
             depfile = dep_file:absolute(),
+            rustflags = rustflags,
+            src = self.src:absolute(),
+            target = out_file:absolute()
         }
     }
     ctx.add_build_step_with_rule(build_step, build_rule)
 end
 
 function Binary:run(args)
-    local result = { self.out:absolute() }
+    local out_file = self:out_file()
+    local result = { out_file:absolute() }
     for _, arg in ipairs(args or {}) do
         table.insert(result, arg)
     end
@@ -76,5 +223,6 @@ function Binary:run(args)
 end
 
 M.Binary = Binary
+M.Library = Library
 
 return M
